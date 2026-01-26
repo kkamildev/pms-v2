51f34fc4c3c4b8350357cbe7ed9f3d333ecd087e
Kamil Kijak <kamil.kijak@pixlesofte.com>
2026-01-26 21:48:36 +0100

    added endpoints for multiple areas operation

--- controllers/areaController.js
+++ controllers/areaController.js
@@ -1,28 +1,45 @@
 
 const LandArea = require("../models/LandArea");
 const withErrorHandling = require("../middlewares/withErrorHandling");
+const sequelize = require("../util/db");
 
-exports.insertArea = withErrorHandling(async (req, res) => {
-    const {idLand, idGroundClass, area} = req.body;
-    await LandArea.create({
-        idLand,
-        idGroundClass,
-        area,
+exports.insertAreas = withErrorHandling(async (req, res) => {
+    const {idLand, areasData} = req.body;
+    await sequelize.transaction(async (t) => {
+        for(const area in areasData) {
+            await LandArea.create({
+                idLand,
+                idGroundClass:area.idGroundClass,
+                area:area.area
+            }, {transaction:t})
+        }
+        res.status(201).json({success:true, message:"Dodano powierzchnie działki"});
     });
-    res.status(201).json({success:true, message:"Dodano powierzchnie działki"});
 });
 
-exports.updateArea = withErrorHandling(async (req, res) => {
-    const {idArea, idGroundClass, area} = req.body;
-    const [affectedRows] = await LandArea.update({
-        idGroundClass,
-        area,
-    }, {where:{id:idArea}});
-    res.status(200).json({success:true, message:"Zaktualizowano powierzchnie działki", affectedRows})
-})
+exports.updateAreas = withErrorHandling(async (req, res) => {
+    const {areasData} = req.body;
+    await sequelize.transaction(async (t) => {
+        let affectedRowsSum = 0;
+        for(const area in areasData) {
+            const [affectedRows] = await LandArea.update({
+                idGroundClass:area.idGroundClass,
+                area:area.area
+            }, {where:{id:area.idArea}, transaction:t})
+            affectedRowsSum += affectedRows;
+        }
+        res.status(201).json({success:true, message:"Dodano powierzchnie działki", affectedRows:affectedRowsSum});
+    });
+});
 
-exports.deleteArea = withErrorHandling(async (req, res) => {
-    const {idArea} = req.body;
-    const deletedCount = await LandArea.destroy({where:{id:idArea}})
-    res.status(200).json({success:true, message:"Usunięto powierzchnie działki", deletedCount})
+exports.deleteAreas = withErrorHandling(async (req, res) => {
+    const {areasIds} = req.body;
+    await sequelize.transaction(async (t) => {
+        let deletedCountSum = 0;
+        for(const id in areasIds) {
+            const deletedCount = await LandArea.destroy({where:{id}, transaction:t})
+            deletedCountSum+= deletedCount;
+        }
+        res.status(200).json({success:true, message:"Usunięto powierzchnie działki", deletedCount:deletedCountSum})
+    });
 });
--- routes/areas.js
+++ routes/areas.js
@@ -16,28 +16,30 @@ router.post("/insert", [
     body("idLand").trim().
     exists({checkFalsy:true}).withMessage("idLand is required").
     isLength({min:21, max:21}).withMessage("idLand must be equal 21 characters"),
-    body("idGroundClass").trim().
-    exists({checkFalsy:true}).withMessage("idGroundClass is required").
-    isLength({min:21, max:21}).withMessage("idGroundClass must be equal 21 characters"),
-    body("area").trim().
-    exists({checkFalsy:true}).withMessage("area is required").
-    isFloat({min:0, max:9999}).withMessage("area must be a positive float value less than 10000").toFloat(),
-], areaController.insertArea);
+    body("areasData").exists({checkFalsy:true}).withMessage("areasData is required").isArray().withMessage("areasData must be an array"),
+    body("areasData.*.idGroundClass").trim().
+    exists({checkFalsy:true}).withMessage("areasData.*.idGroundClass is required").
+    isLength({min:21, max:21}).withMessage("areasData.*.idGroundClass must be equal 21 characters"),
+    body("areasData.*.area").trim().
+    exists({checkFalsy:true}).withMessage("areasData.*.area is required").
+    isFloat({min:0, max:9999}).withMessage("areasData.*.area must be a positive float value less than 10000").toFloat(),
+], areaController.insertAreas);
 
 router.put("/update", [
-    body("idArea").trim().
-    exists({checkFalsy:true}).withMessage("idArea is required"),
-    body("idGroundClass").trim().
-    exists({checkFalsy:true}).withMessage("idGroundClass is required").
-    isLength({min:21, max:21}).withMessage("idGroundClass must be equal 21 characters"),
-    body("area").trim().
-    exists({checkFalsy:true}).withMessage("area is required").
-    isFloat({min:0, max:9999}).withMessage("area must be a positive float value less than 10000").toFloat(),
-], areaController.updateArea);
+    body("areasData").exists({checkFalsy:true}).withMessage("areasData is required").isArray().withMessage("areasData must be an array"),
+    body("areasData.*.idArea").trim().
+    exists({checkFalsy:true}).withMessage("areasData.*.idArea is required"),
+    body("areasData.*.idGroundClass").trim().
+    exists({checkFalsy:true}).withMessage("areasData.*.idGroundClass is required").
+    isLength({min:21, max:21}).withMessage("areasData.*.idGroundClass must be equal 21 characters"),
+    body("areasData.*.area").trim().
+    exists({checkFalsy:true}).withMessage("areasData.*.area is required").
+    isFloat({min:0, max:9999}).withMessage("areasData.*.area must be a positive float value less than 10000").toFloat(),
+], areaController.updateAreas);
 
 router.delete("/delete", [
-    body("idArea").trim().
-    exists({checkFalsy:true}).withMessage("idArea is required")
-], areaController.deleteArea);
+    body("areasIds").exists({checkFalsy:true}).withMessage("areasIds is required").isArray().withMessage("areasIds must be an array"),
+    body("areasIds.*.id").trim().exists({checkFalsy:true}).withMessage("areasIds.*.id is required")
+], areaController.deleteAreas);
 
 module.exports = router;
